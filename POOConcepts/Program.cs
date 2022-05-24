dd388a5e4413488d42957d009b011e9eb88eb699
jcastro <joseph.castro.ing@gmail.com>
2022-05-24 17:00:28 -0500

    Add project files.

---
+++ POOConcepts/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2019, 10, 3));
+    Console.WriteLine(new Date(2022, 11, 30));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
+
